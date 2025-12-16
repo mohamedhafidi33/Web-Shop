cef4ec0ab9c1ffbbdedb3c592f6776b48c9b2184
Mohamed Hafidi <mohamed.hafidi.mh@gmail.com>
2025-12-16 23:30:11 +0100

    Consuming from orders.error

--- src/main/java/de/fhaachen/si/web/shop/rabbitmq/consumer/RabbitMQConsumer.java
+++ src/main/java/de/fhaachen/si/web/shop/rabbitmq/consumer/RabbitMQConsumer.java
@@ -1,6 +1,9 @@
 package de.fhaachen.si.web.shop.rabbitmq.consumer;
 
+import java.util.Map;
+
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,5 +15,13 @@ public class RabbitMQConsumer {
 		System.out.println("ERP Response Received: " + response);
 
 		// TODO
+	}
+	
+	@RabbitListener(queues = "${app.queues.order-error}")
+	public void processFailedOrders(String failedOrderJson, @Header(required = false) Map<String, Object> headers) {
+	    System.err.println("Admin Alert: Order failed processing.");
+	    System.err.println("Payload: " + failedOrderJson);
+	    
+	    // Logic: Save to a database table "FAILED_ORDERS" for an admin dashboard
 	}
 }
